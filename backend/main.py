aed08f88016c4488a79188f7b907394e4453e89c
fatemegh129 <87355455+fatemegh129@users.noreply.github.com>
2026-06-02 16:15:29 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-
+from routers import auth
 app = FastAPI(title="Relay Backend")
-
+app.include_router(auth.router)
 @app.get("/")
 def read_root():
     return "Backend läuft!"
