e155359cd19c46af33eb7bda7db07a85df8a85c6
Sobhan Haerizadeh <sobhanhaerizade@gmail.com>
2026-06-03 10:02:04 +0200

    feat: implement form validation and error handling in Register component

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from 'react'
+
 function Register() {
+
+    const [error, setError] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    function handleSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            setError('Passwörter stimmen nicht überein! ');
+        }
+        else {
+            setError('');
+        }
+
+        if (email.length === 0) {
+            setError('E-Mail darf nicht leer sein! ');
+        }
+
+    }
+
+
+    useEffect(() => {
+        if (confirmPassword && password !== confirmPassword && password.length > 0) {
+            setError('Passwörter stimmen nicht überein! ');
+        }
+        else {
+            setError('');
+        }
+    }, [confirmPassword, password]);
+
+
     return (
         <>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full md:w-auto md:ml-4 md:mt-4">
@@ -9,13 +44,14 @@ function Register() {
             <div className="min-h-screen bg-gray-950 flex items-center justify-center">
                 <div className="bg-gray-900 p-8 w-96 rounded-lg">
                     <h1 className="text-white text-2xl font-bold mb-6">Register</h1>
-                    <form className="flex flex-col gap-4">
-                        <input type="email" placeholder="E-Mail" className="bg-gray-800 text-white p-2 rounded" />
-                        <input type="password" placeholder="Password" className="bg-gray-800 text-white p-2 rounded" />
-                        <input type="password" placeholder="Confirm Password" className="bg-gray-800 text-white p-2 rounded" />
+                    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                        <input type="email" placeholder="E-Mail" className="bg-gray-800 text-white p-2 rounded" onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" placeholder="Password" className="bg-gray-800 text-white p-2 rounded" onChange={(e) => setPassword(e.target.value)} />
+                        <input type="password" placeholder="Confirm Password" className="bg-gray-800 text-white p-2 rounded" onChange={(e) => setConfirmPassword(e.target.value)} />
                         <button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                             Register
                         </button>
+                        <p className="text-red-500 text-sm mt-2 font-bold">{error}</p>
                     </form>
                 </div>
             </div>
