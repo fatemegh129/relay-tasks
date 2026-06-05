be94848354eaf018bf83020d43bccfcfb38fcaed
fatemegh129 <87355455+fatemegh129@users.noreply.github.com>
2026-06-05 13:52:36 +0200

    Update auth.py

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -4,14 +4,12 @@ router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
 )
+
 @router.post("/register")
 def register_mock():
     return {"message": "Registrierung erfolgreich"}
 
-
 @router.post("/login")
-
-
 def login_mock():
 
     return "Login erfolgreich"
